34eedff83b9bd331dd9deee0a7057d978388c3d5
Reid Chu <e079004@gmail.com>
2021-02-22 22:10:36 +0800

    ejs fixed

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const ejs = require("ejs")
 
-app.listen(3000, function (e) {
-    console.log("This site is running on port 3000")
-})
+app.listen(3000, function () {
+    console.log("running on port 3000")
+});
+app.set("view engine", "ejs");  //ejs engine will check "views" folder by default
+app.use(express.static("public"));
 
-app.get("/hello", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-})
+
+app.get("/", function (req, res) {
+    res.render("index", { 1: 1 })
+});
