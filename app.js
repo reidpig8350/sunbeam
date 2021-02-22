07956d1627d670da3dcc630af104bb8051dd6ea0
ReidChu <e079004@gmail.com.tw>
2021-02-22 23:42:51 +0800

    cancel example.com

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs")
 
+const port = 3000
 app.listen(3000, function () {
-    console.log("running on port 3000")
+    console.log("http://localhost:" + port + "/")
 });
 app.set("view engine", "ejs");  //ejs engine will check "views" folder by default
 app.use(express.static("public"));
