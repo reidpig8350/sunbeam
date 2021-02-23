9fb0b1733784d60a607e20195c9e25f178527d3e
ReidChu <e079004@gmail.com.tw>
2021-02-24 00:03:08 +0800

    contact about articles

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs")
 
-const port = 3000
-app.listen(3000, function () {
+const port = 2000
+app.listen(port, function () {
     console.log("http://localhost:" + port + "/");
 });
 app.set("view engine", "ejs");  //ejs engine will check "views" folder by default
 app.use(express.static("public"));
 
 
+//--------------------GET--------------------//
 app.get("/", function (req, res) {
     res.render("index", {})
 });
+
+app.get("/about", function (req, res) {
+    res.render("about", {})
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact", {})
+});
+
+app.get("/portfolio/:articles", function (req, res) {
+    res.redirect("/")
+    console.log(req.params.articles)
+})
+//--------------------GET--------------------//
