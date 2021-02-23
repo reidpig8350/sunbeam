bcf3130e0494a676bd54d03feabdd2f9f562663a
ReidChu <e079004@gmail.com.tw>
2021-02-23 22:00:21 +0800

    GTM installed

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const ejs = require("ejs")
 
 const port = 3000
 app.listen(3000, function () {
-    console.log("http://localhost:" + port + "/")
+    console.log("http://localhost:" + port + "/");
 });
 app.set("view engine", "ejs");  //ejs engine will check "views" folder by default
 app.use(express.static("public"));
 
 
 app.get("/", function (req, res) {
-    res.render("index", { 1: 1 })
+    res.render("index", {})
 });
